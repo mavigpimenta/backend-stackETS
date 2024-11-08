b5ab49b6437a40654111828e512f56e17c2d2951
JoviOakz <joao.mateussdc@gmail.com>
2024-11-08 08:37:48 -0300

    fix: db_url

--- src/startup/db.js
+++ src/startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-  const db = config.get("db");
+  const db = process.env.DB_URL;
   mongoose
     .connect(db, {  })
     .then(() => console.log(`connected to ${db}`));
